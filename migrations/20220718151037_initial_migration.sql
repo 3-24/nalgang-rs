5b730ab0da886e7bfb35ab7be7b2895cc0c12ff6
Youngseok Choe <dev.youngseok@gmail.com>
2022-07-19 00:56:18 +0900

    Add sqlx initial migration

---
+++ migrations/20220718151037_initial_migration.sql
@@ -0,0 +1,23 @@
+-- Add migration script here
+CREATE TABLE IF NOT EXISTS Members
+(
+    id integer NOT NULL,
+    guild integer NOT NULL,
+    score integer,
+    combo integer
+)
+
+CREATE TABLE IF NOT EXISTS Attendances
+(
+    id integer NOT NULL,
+    guild integer NOT NULL,
+    hit_message nvarchar,
+    hit_timestamp float NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS AttendanceTimeCount
+(
+    guild integer NOT NULL,
+    hit_count integer NOT NULL,
+    hit_timestamp float NOT NULL
+)
